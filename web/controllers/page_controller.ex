cbb4d7e9475bea2ad31c0ae76d57661dfc435902
Syed Ali Abbas Shah (01322) <ali.abbas@nxb.com.pk>
2017-04-03 16:27:26 +0500

    recent posts

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule BlogTest.PageController do
   plug :put_layout, "admin.html"
 
   def index(conn, _params) do
-    render conn, "index.html"
+    recent_posts = BlogTest.Repo.all from p in BlogTest.Post, order_by: [desc: p.updated_at],limit: 2
+    render conn, "index.html",recent_posts: recent_posts
   end
 end
